aa087adafca2bf6c5f2d1407a796b6fc5cf658ea
barisyildirimmm <barisyildirimmm@gmail.com>
2012-09-21 02:12:10 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aztutar::Application.config.secret_token = '5a88b2a5b04bf5702385cc07ff6224f4b5dbf1b3cfdd87bb2f39a9e3f062c8d7fb54d4be70b4d6c9e22ae3c917d097b8e392692377ef111345ad8e6e61efd4ff'

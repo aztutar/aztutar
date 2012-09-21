0b273bc7053f495b28bc207bb6e4198eb2f3dc9f
barisyildirimmm <barisyildirimmm@gmail.com>
2012-09-21 03:43:23 +0300

    Ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Aztutar::Application.routes.draw do
+  resources :welcomes
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,8 +50,7 @@ Aztutar::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  root :to => 'welcomes#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
